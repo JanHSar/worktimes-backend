4e93a0d276372e16db4d948c1c3fd39aab4d7dd1
Jan Sarstedt <sarstedt.jan@gmail.com>
2024-01-03 14:24:28 +0100

    FIX typo

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,7 @@
 module.exports = async (req, res, next) => {
-
   // Get user by xApiKey
   const xApiKey = req.headers['x-api-key'];
-  if (xApiKey && xApiKey.lenght > 3) {
+  if (xApiKey && xApiKey.length > 3) {
     try {
       const user = await req.db.user.findOne({
         where: {xApiKey}
@@ -11,7 +10,6 @@ module.exports = async (req, res, next) => {
       // Add user to request
       req.user = user;
       res.status(200);
-      
       return next();
     } catch (err) {
       console.log(err);
