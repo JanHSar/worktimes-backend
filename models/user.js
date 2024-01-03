64da382dec5881041c5495e7962450300472892a
Jan Sarstedt <sarstedt.jan@gmail.com>
2024-01-03 11:17:36 +0100

    Add user log in

--- models/user.js
+++ models/user.js
@@ -17,6 +17,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'user',
-  });
+  }
+  );
   return user;
 };
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const bcrypt = require('bcrypt');
 var router = express.Router();
 
 /* GET users listing. */
@@ -8,5 +9,46 @@ router.get('/', async function(req, res, next) {
   })
   res.send(users);
 });
+
+/**
+ * Log find and log in a user
+ */
+router.post('/auth', async function(req, res, next) {
+  try {
+    // Find User with credentials
+    const user = await req.db.user.findOne({
+      where: {
+        name: req.body.userName,
+        password: req.body.password
+      }
+    });
+
+    if(user) {
+      // Generate xApiKey
+      const xApiKey = await generateXApiKey();
+      // Save key to db
+      await user.update({xApiKey: xApiKey});
+      await user.save();
+
+      res.send(user);
+    } else {
+      res.status(401).send('Unauthorized');
+    }
+
+  } catch (err) {
+    console.log(err);
+    res.status(401).send('Unauthorized');
+  }
+})
+
+/**
+ * Generates random hased string
+ * @returns xApiKey 
+ */
+async function generateXApiKey() {
+  const token = crypto.randomUUID();
+  const xApiKey = await bcrypt.hash(token, 6);
+  return xApiKey;
+}
 
 module.exports = router;
